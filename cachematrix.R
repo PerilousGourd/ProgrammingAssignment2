6fbe851026827c6a896a99c20f0c1b3f3839eac3
PerilousGourd <PerilousGourd@outlook.com>
2015-02-20 22:00:24 +1000

    Wrapped lines

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,13 @@
-# These two functions, for a given matrix, caches and returns its inverse. If the inverse matrix has previously been calculated, it is retrieved from the cache (not calculated again).
+# These two functions, for a given matrix, caches and returns its inverse.
+# If the inverse matrix has previously been calculated, it is retrieved from
+# the cache (not calculated again).
 
 # Defines the functions necessary for cacheSolve to work
 makeCacheMatrix <- function(x = matrix()) {
    m <- NULL
    
-   # Defines a function that, when called, will set x to the parameter it is called on, and clear any prior value of m.
+   # Defines a function that, when called, will set x to the parameter it
+   # is called on, and clear any prior value of m.
    set <- function(y) {
       x <<- y
       m <<- NULL
@@ -30,16 +33,19 @@ makeCacheMatrix <- function(x = matrix()) {
 # Returns a matrix that is the inverse of 'x'
 cacheSolve <- function(x, ...) {
 
-   # Set m to the cached inverse of 'x' (or NULL if it hasn't been calculated)
+   # Set m to the cached inverse of 'x' (or NULL if it hasn't been
+   # calculated)
    m <- x$getInverse()
    
    # If the cache isn't empty, return its contents (the inverse of 'x')
    if(!is.null(m)) {
       message("getting cached data")
-      return(m)     # (If the inverse had been cached, the function prints the inverse and exits here)
+      return(m)     # (If the inverse had been cached, the function
+      # prints the inverse and exits here)
    }
    
-   # To reach here, the cache must have been empty. Calculate and cache the inverse now.
+   # To reach here, the cache must have been empty. Calculate and cache
+   # the inverse now.
    data <- x$get()
    m <- solve(data, ...)
    x$setInverse(m)
